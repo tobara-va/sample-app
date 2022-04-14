3b45ac6c46ec1158a8621fa5ff21e07dbba8f99d
Tyler Obara <tylerobara@Tylers-MacBook-Pro.local>
2022-04-14 19:55:03 -0400

    gotta fix the quotes

--- scripts/get-tag.sh
+++ scripts/get-tag.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export TAG=$(grep 'Welcome to nginx ' ../index.html | awk '{print $4}' | awk -F '!' '{print $1}')
+export TAG=$(grep 'Welcome to nginx ' index.html | awk '{print $4}' | awk -F '!' '{print $1}')
+
+echo ${TAG}
 
