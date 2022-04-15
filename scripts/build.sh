3e627f7c33fcc3386129b4fe66f83500eb94cafc
Tyler Obara <tylerobara@Tylers-MacBook-Pro.local>
2022-04-14 20:07:04 -0400

    gotta fix the quotes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 export TAG=$(grep 'Welcome to nginx ' index.html | awk '{print $4}' | awk -F '!' '{print $1}')
 
-docker build -t sample-app:${TAG}  sample-app:dev .
+docker build -t sample-app:${TAG} -t sample-app:dev .
