db877edd9e73b805868307949b44403420b0acf7
Tyler Obara <tylerobara@Tylers-MacBook-Pro.local>
2022-04-18 09:10:45 -0400

    trying a script file for dive

--- scripts/dive.sh
+++ scripts/dive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock jauderho/dive "--ci sample-app:dev" >> sample-app-dev.txt
+docker run -v /var/run/docker.sock:/var/run/docker.sock jauderho/dive --ci sample-app:dev >> sample-app-dev.txt
